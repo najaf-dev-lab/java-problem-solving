e2cbec314455636db003e74896edecd4b8066503
Najaf Dev Lab <najafalisolangi58@gmail.com>
2026-09-04 23:29:07 -0700

    Add files via upload

---
+++ basics/OddCounter.java
@@ -0,0 +1,27 @@
+package practice;
+import java.util.Scanner;
+
+public class OddCounter {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter start number: ");
+        int start = input.nextInt();
+
+        System.out.print("Enter end number: ");
+        int end = input.nextInt();
+
+        int count = 0;
+
+        for (int i = start; i <= end; i++) {
+            if (i % 2 != 0) {
+                count++;
+            }
+        }
+
+        System.out.println("Total odd numbers: " + count);
+        
+        input.close();
+    }
+}
